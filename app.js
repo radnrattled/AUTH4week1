af86338afae35618e604912e24a10b8f20a72695
Megan Morris <megs@Mac-mini.local>
2021-01-19 13:20:38 -0500

    .

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http'); // like a PHP require
-
+const stream = require('fs');
 // require is more or less the same as a JS import
 
 //const hostname = '127.0.0.1'; // this is localhost
@@ -8,7 +8,36 @@ const port = process.env.PORT || 3000; // localhost:3000
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('8 days and counting til the World freakin parties cuz the orange idiot will be gone!');
+
+    let url = req.url;
+    switch (url) {
+        case "/contact":
+            stream.readFile('contact.html', null, ((err, data) =>{
+               if (err) {
+                   res.writeHead(404);
+                   res.write('404 Not found')
+                
+               } else {
+                   res.write(data);
+               } 
+            }))
+        break;
+        case "/portfolio":
+            stream.readFile('portfolio.html', null, ((err, data) =>{
+               if (err) {
+                   res.writeHead(404);
+                   res.write('404 Not found')
+                
+               } else {
+                   res.write(data);
+               } 
+            }))
+        break;
+
+        default:
+            res.end('8 days and counting til the World freakin parties cuz the orange idiot will be gone!');
+    }
+    
 });
 
 server.listen(port, () => {
